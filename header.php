6aa583a1bc335e53f818d55601de20ffa9b6173b
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-07-11 13:32:34 +0530

    header

---
+++ header.php
@@ -0,0 +1,38 @@
+<header>
+    <div class="header01">
+        <div class="header02">
+            <h3>Welcome to Sun Champ Global Trades !</h3>
+        </div>
+        <div class="header03">
+            <h3 style="padding-top:.19rem ;">Follow Us On:</h3>
+           <a href=""> <img src="img/facebook.svg" alt=""></a>
+           <a href=""><img src="img/twitter.svg" alt=""></a>
+            <a href=""><img src="img/link.svg" alt=""></a>
+            <a href="">
+                <img src="img/insta.svg" alt=""></a>
+        </div>
+
+    </div>
+    <nav>
+        <div class="logo">
+            <img src="img/logo.svg" alt="">
+        </div>
+        <div class="manus">
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">ABOUT US</a></li>
+                <li><a href="#">TEXTILE PRODUCTS</a></li>
+                <li><a href="#">NEWS / BLOGS</a></li>
+            </ul>
+
+        </div>
+
+        <div class="contactbtn">
+            <a id="contactbtn" href="">Contact Us </a>
+            <img src="img/skip.svg" alt="">
+        </div>
+
+
+
+    </nav>
+</header>
