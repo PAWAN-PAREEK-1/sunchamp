58d171dc3265eb1008a7d93b103f2f0745dd7945
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-07-13 10:47:37 +0530

    specilization

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,24 +8,76 @@
     <script src="https://kit.fontawesome.com/5f77a54189.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
+
 <body>
-   <?php
+    <?php
     include "header.php";
    ?>
 
-<section class="main">
-    <section class="hero">
+    <section class="main">
+        <section class="hero">
             <div class="main-container">
-                    <img src="img/clipimg.svg" alt="">
-                    <h3>We help businesses</h3>
-                    <h1>Grow and Innovate</h1>
-                    <p>Armed with Sun Champ Global Trades insight and foresight.</p>
-                    <p>Business Leaders will find us trustworthy, helping achieve desired results.</p>
-                    <div class="contactbtn indexhero">
-                        <a id="contactbtn01" href="">Contact Us </a>
-                        <img src="img/skip.svg" alt="" id="contactbtnimg">
-                    </div>
+                <img src="img/clipimg.svg" alt="">
+                <h3>We help businesses</h3>
+                <h1>Grow and Innovate</h1>
+                <p>Armed with Sun Champ Global Trades insight and foresight.</p>
+                <p>Business Leaders will find us trustworthy, helping achieve desired results</p>
+                <div class="contactbtn indexhero">
+                    <a id="contactbtn01" href="">Contact Us </a>
+                    <img src="img/skip.svg" alt="" id="contactbtnimg">
+                </div>
             </div>
+        </section>
+
+
+
+        <section class="specelity">
+            <div class="spheading">
+                <h1>We Give Top Production From Every Angle</h1>
+            </div>
+
+            <div class="specelity-testi">
+                <div class="spbox">
+                    <div class="spbox1">
+                        <img src="img/sp1.svg" alt="">
+                        <h1>01</h1>
+                    </div>
+                    <hr>
+                    <p>Choose For Many Fabrics and Textiles materials for your production</p>
+                    <img src="img/spdesign.svg" alt="" id="spdesign">
+                </div>
+                <div class="spbox">
+                    <div class="spbox1">
+                        <img src="img/sp2.svg" alt="">
+                        <h1>02</h1>
+                    </div>
+                    <hr>
+                    <p>Choose For Many Fabrics and Textiles materials for your production</p>
+                    <img src="img/spdesign.svg" alt="" id="spdesign">
+                </div>
+                <div class="spbox">
+                    <div class="spbox1">
+                        <img src="img/sp3.svg" alt="">
+                        <h1>03</h1>
+                    </div>
+                    <hr>
+                    <p>Choose For Many Fabrics and Textiles materials for your production</p>
+                    <img src="img/spdesign.svg" alt="" id="spdesign">
+                </div>
+            </div>
+        </section>
+
+
+
+
+
+
+
+
+
+
+
+
     </section>
 
 
@@ -37,23 +90,11 @@
 
 
 
-
-</section>
-
-
-
-
-
-
-
-
-
-
-
-<?php
+    <?php
     include "footer.php";
    ?>
 
-<script src="script.js"></script>
+    <script src="script.js"></script>
 </body>
+
 </html>
