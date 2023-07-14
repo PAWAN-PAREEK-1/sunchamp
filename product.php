130da5b51707a40ebf6f573bff75c49ce8396545
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-07-14 14:54:10 +0530

    product section complete

---
+++ product.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/5f77a54189.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+    include "header.php";
+   ?>
+
+    <section class="all-hero">
+        <h1>Products</h1>
+        <img src="img/clipimg2.svg" alt="">
+    </section>
+    <section class="ourproduct">
+        <h3>Our Product</h3>
+        <h1>Let's Experience Exclusive Quality</h1>
+
+        <div class="our-main">
+
+            <div class="our-main-p">
+                <div class="our-main01">
+                    <img src="img/ourp1.svg" alt="">
+                    <h1>Polyster</h1>
+                </div>
+                <p>Polyester fabrics are known for their excellent wrinkle resistance. They ten d to retain.</p>
+                <h4>READ MORE &nbsp;&nbsp;+</h4>
+                <img src="img/icon3.svg" alt="" id="our-ser-img">
+            </div>
+            <div class="our-main-p">
+                <div class="our-main01">
+                    <img src="img/icon4.svg" alt="">
+                    <h1>Polyster</h1>
+                </div>
+                <p>Polyester fabrics are known for their excellent wrinkle resistance. They ten d to retain.</p>
+                <h4>READ MORE &nbsp;&nbsp;+</h4>
+                <img src="img/icon3.svg" alt="" id="our-ser-img">
+            </div>
+            <div class="our-main-p">
+                <div class="our-main01">
+                    <img src="img/icon5.svg" alt="">
+                    <h1>Polyster</h1>
+                </div>
+                <p>Polyester fabrics are known for their excellent wrinkle resistance. They ten d to retain.</p>
+                <h4>READ MORE &nbsp;&nbsp;+</h4>
+                <img src="img/icon3.svg" alt="" id="our-ser-img">
+            </div>
+
+        </div>
+        <div class="our-main our-main2">
+
+            <div class="our-main-p">
+                <div class="our-main01">
+                    <img src="img/icon6.svg" alt="">
+                    <h1>Polyster</h1>
+                </div>
+                <p>Polyester fabrics are known for their excellent wrinkle resistance. They ten d to retain.</p>
+                <h4>READ MORE &nbsp;&nbsp;+</h4>
+                <img src="img/icon3.svg" alt="" id="our-ser-img">
+            </div>
+            <div class="our-main-p">
+                <div class="our-main01">
+                    <img src="img/icon7.svg" alt="">
+                    <h1>Polyster</h1>
+                </div>
+                <p>Polyester fabrics are known for their excellent wrinkle resistance. They ten d to retain.</p>
+                <h4>READ MORE &nbsp;&nbsp;+</h4>
+                <img src="img/icon3.svg" alt="" id="our-ser-img">
+            </div>
+            <div class="our-main-p">
+                <div class="our-main01">
+                    <img src="img/icon8.svg" alt="">
+                    <h1>Polyster</h1>
+                </div>
+                <p>Polyester fabrics are known for their excellent wrinkle resistance. They ten d to retain.</p>
+                <h4>READ MORE &nbsp;&nbsp;+</h4>
+                <img src="img/icon3.svg" alt="" id="our-ser-img">
+            </div>
+
+        </div>
+    </section>
+
+
+
+
+    <section class="products">
+        <div class="pro-head">
+            <img src="img/icon9.svg" alt="">
+            <h1>Polyster</h1>
+            <img src="img/icon3.svg" alt="" id="icon3">
+        </div>
+
+        <div class="pro-dec">
+            <div class="pro-img">
+                <img src="img/productSec.svg" alt="">
+
+            </div>
+            <div class="mission mis">
+
+                <h1>Service..... that builds Trust</h1>
+                <p>The mission of the Sun Champ Global Trades is to create prosperity by strengthening the
+                    competitiveness of industry, promoting trade and investment, and ensuring fair trade and
+                    compliance with trade laws and agreements.</p>
+                <p>We strive to be a resourceful foreign trade manual for our stakeholders in our collective journey
+                    of transparent and trustworthy international sale transactions.</p>
+                <p>The goal of Sun Champ Global Trades is to establish international business relationship through
+                    facilitating trade opportunities, creating joint ventures and strategic alliances between
+                    international businesses from various countries.</p>
+            </div>
+        </div>
+
+        <div class="points">
+            <div class="point1">
+                <h4><img src="img/icon2.svg" alt="">Trends the Current Market in Design.</h4>
+                <h4><img src="img/icon2.svg" alt="">The rayon fabrics wholesaler is available.</h4>
+                <h4><img src="img/icon2.svg" alt="">The Global fabrics distributor is done by company..</h4>
+            </div>
+            <div class="point1">
+                <h4><img src="img/icon2.svg" alt="">Trends the Current Market in Design</h4>
+                <h4><img src="img/icon2.svg" alt="">The Global fabrics distributor is done by company.</h4>
+                <h4><img src="img/icon2.svg" alt="">The rayon fabrics wholesaler is available.</h4>
+            </div>
+
+        </div>
+    </section>
+
+</body>
+
+</html>
