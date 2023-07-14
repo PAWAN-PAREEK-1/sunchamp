9560fd09a34a2970750d2f51ba2b6e4fcd374745
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-07-14 16:59:15 +0530

    blog section

---
+++ blog.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/5f77a54189.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    include "header.php";
+   ?>
+    <section class="all-hero">
+        <h1>Latest News & Blog</h1>
+        <img src="img/clipimg2.svg" alt="">
+    </section>
+
+    <section class="news-blog-hero">
+        <h3>Latest News & Blog</h3>
+        <h1>We believe in counting numbers</h1>
+
+
+        <div class="news1">
+            <div class="news2">
+                <div class="news-box news0101">
+
+                    <div class="news-img">
+                        <h3>Traditional</h3>
+                        <img src="img/news5.svg" alt="">
+
+                    </div>
+                    <div class="news-details">
+                        <div class="news-time news-time2">
+                            <h5><img src="img/person.svg" alt="">By: John Doe</h5>
+                            <h5><img src="img/calender.svg" alt="">9 June, 2022</h5>
+                        </div>
+                        <h2>Strategies For Reducing <br>Environmental</h2>
+                        <p>It is a long established fact that a rea der will be distracted by the readable content of a
+                            page when looking at its layout.</p>
+                           <a href=""> <h4>READ MORE  <img src="img/rightarrow.svg" alt=""></h4></a>
+                    </div>
+                </div>
+                <div class="news-box news0101">
+
+                    <div class="news-img">
+                        <h3>Traditional</h3>
+                        <img src="img/news5.svg" alt="">
+
+                    </div>
+                    <div class="news-details">
+                        <div class="news-time news-time2">
+                            <h5><img src="img/person.svg" alt="">By: John Doe</h5>
+                            <h5><img src="img/calender.svg" alt="">9 June, 2022</h5>
+                        </div>
+                        <h2>Strategies For Reducing <br>Environmental</h2>
+                        <p>It is a long established fact that a rea der will be distracted by the readable content of a
+                            page when looking at its layout.</p>
+                           <a href=""> <h4>READ MORE  <img src="img/rightarrow.svg" alt=""></h4></a>
+                    </div>
+                </div>
+                <div class="news-box news0101">
+
+                    <div class="news-img">
+                        <h3>Traditional</h3>
+                        <img src="img/news5.svg" alt="">
+
+                    </div>
+                    <div class="news-details">
+                        <div class="news-time news-time2">
+                            <h5><img src="img/person.svg" alt="">By: John Doe</h5>
+                            <h5><img src="img/calender.svg" alt="">9 June, 2022</h5>
+                        </div>
+                        <h2>Strategies For Reducing <br>Environmental</h2>
+                        <p>It is a long established fact that a rea der will be distracted by the readable content of a
+                            page when looking at its layout.</p>
+                           <a href=""> <h4>READ MORE  <img src="img/rightarrow.svg" alt=""></h4></a>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
