7a6b44fed4738d1e5aa6ee3c16cd19ff7a448df6
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-07-15 17:55:11 +0530

    news page

---
+++ news.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/5f77a54189.js" crossorigin="anonymous"></script>
+        <title>Sunchamp</title>
+
+    </head>
+<body>
+
+    <?php
+    include "header.php";
+   ?>
+
+
+   <section class="all-hero">
+    <h1>News</h1>
+    <img src="img/clipimg2.svg" alt="">
+</section>
+
+
+<section class="news-blog-hero  news-det-dec">
+    <h3>Latest News & Blog</h3>
+    <h1>We believe in counting numbers</h1>
+
+</section>
+
+
+
+
+   <?php
+   include "footer.php";
+  ?>
+
+
+</body>
+</html>
