8d7cfa489fbf0ef2a56178c3bbace014b812a12a
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-07-31 12:46:38 +0530

    toogle update

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
         </div>
 
 
-        <div class="manus " >
+        <div class="manus active1" >
             <ul>
                 <li><a href="index.php" <?php if (basename($_SERVER['PHP_SELF']) == 'index.php') echo 'class="active"'; ?>>HOME</a></li>
                 <li><a href="about.php" <?php if (basename($_SERVER['PHP_SELF']) == 'about.php') echo 'class="active"'; ?>>ABOUT US</a></li>
@@ -46,23 +46,43 @@
             <img src="img/skip.svg" alt="" id="contactbtnimg">
         </div>
 
-
-
         <script>
-            document.addEventListener("DOMContentLoaded", function () {
-
+  document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.querySelector(".hamburger");
     const manus = document.querySelector(".manus");
     const contactBtn = document.querySelector(".contactbtn");
 
-    hamburger.addEventListener("click", function () {
-        hamburger.classList.toggle("active");
-        manus.classList.toggle("active");
-        contactBtn.classList.toggle("active");
-    });
-});
+    // Function to close the menu
+    function closeMenu() {
+      hamburger.classList.remove("active");
+      manus.classList.remove("active");
+      contactBtn.classList.remove("active");
+      window.removeEventListener("scroll", closeMenu);
+    }
 
-        </script>
+    hamburger.addEventListener("click", function () {
+      hamburger.classList.toggle("active");
+      manus.classList.toggle("active");
+      contactBtn.classList.toggle("active");
+
+      if (manus.classList.contains("active")) {
+        // If the menu is now active, add a scroll event listener to the window to close it when scrolled
+        window.addEventListener("scroll", closeMenu);
+      } else {
+        // If the menu is not active, remove the scroll event listener from the window
+        window.removeEventListener("scroll", closeMenu);
+      }
+    });
+
+    manus.addEventListener("click", function () {
+      // Scroll to the top of the page
+      window.scrollTo(0, 0);
+      // Deactivate the menu
+      closeMenu();
+    });
+  });
+</script>
+
 
     </nav>
 </header>
